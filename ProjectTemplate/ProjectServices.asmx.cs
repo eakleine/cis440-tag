8bd013c863a231ca9fa7c5340ef2c05acf9513e7
HUANG <huangyu-hsuan@huangyuuandembp.3500.dhcp.asu.edu>
2023-02-14 16:26:27 -0700

    added C# project and sln

---
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProjectTemplate
+{
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	[System.Web.Script.Services.ScriptService]
+
+	public class ProjectServices : System.Web.Services.WebService
+	{
+		////////////////////////////////////////////////////////////////////////
+		///replace the values of these variables with your database credentials
+		////////////////////////////////////////////////////////////////////////
+		private string dbID = "sprc2023team11";
+		private string dbPass = "sprc2023team11";
+		private string dbName = "sprc2023team11";
+		////////////////////////////////////////////////////////////////////////
+		
+		////////////////////////////////////////////////////////////////////////
+		///call this method anywhere that you need the connection string!
+		////////////////////////////////////////////////////////////////////////
+		private string getConString() {
+			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
+		}
+		////////////////////////////////////////////////////////////////////////
+
+
+
+		/////////////////////////////////////////////////////////////////////////
+		//don't forget to include this decoration above each method that you want
+		//to be exposed as a web service!
+		[WebMethod(EnableSession = true)]
+		/////////////////////////////////////////////////////////////////////////
+		public string TestConnection()
+		{
+			try
+			{
+				string testQuery = "select * from user_testing";
+
+				////////////////////////////////////////////////////////////////////////
+				///here's an example of using the getConString method!
+				////////////////////////////////////////////////////////////////////////
+				MySqlConnection con = new MySqlConnection(getConString());
+				////////////////////////////////////////////////////////////////////////
+
+				MySqlCommand cmd = new MySqlCommand(testQuery, con);
+				MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+				DataTable table = new DataTable();
+				adapter.Fill(table);
+				return "Success!";
+			}
+			catch (Exception e)
+			{
+				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
+			}
+		}
+		
+		//EXAMPLE OF A SIMPLE SELECT QUERY (PARAMETERS PASSED IN FROM CLIENT)
+		[WebMethod(EnableSession = true)] //NOTICE: gotta enable session on each individual method
+		public bool LogOn(string uid, string pass)
+		{
+			//we return this flag to tell them if they logged in or not
+			bool success = false;
+
+			//our connection string comes from our web.config file like we talked about earlier
+			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+			//here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
+			//NOTICE: we added admin to what we pull, so that we can store it along with the id in the session
+			// string sqlSelect = "SELECT id, admin FROM accounts WHERE userid=@idValue and pass=@passValue";
+			string sqlSelect = "SELECT id FROM user_database WHERE userid=@idValue and pass=@passValue";
+			
+			//set up our connection object to be ready to use our connection string
+			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+			//set up our command object to use our connection, and our query
+			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+			//tell our command to replace the @parameters with real values
+			//we decode them because they came to us via the web so they were encoded
+			//for transmission (funky characters escaped, mostly)
+			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
+			sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
+
+			//a data adapter acts like a bridge between our command object and 
+			//the data we are trying to get back and put in a table object
+			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+			//here's the table we want to fill with the results from our query
+			DataTable sqlDt = new DataTable();
+			//here we go filling it!
+			sqlDa.Fill(sqlDt);
+			//check to see if any rows were returned.  If they were, it means it's 
+			//a legit account
+			if (sqlDt.Rows.Count > 0)
+			{
+				//if we found an account, store the id and admin status in the session
+				//so we can check those values later on other method calls to see if they 
+				//are 1) logged in at all, and 2) and admin or not
+				Session["id"] = sqlDt.Rows[0]["id"];
+				// Session["admin"] = sqlDt.Rows[0]["admin"];
+				success = true;
+			}
+			//return the result!
+			return success;
+		}
+		
+		
+		//EXAMPLE OF A SELECT, AND RETURNING "COMPLEX" DATA TYPES
+		[WebMethod(EnableSession = true)]
+		public Account[] GetAccounts()
+		{
+			//check out the return type.  It's an array of Account objects.  You can look at our custom Account class in this solution to see that it's 
+			//just a container for public class-level variables.  It's a simple container that asp.net will have no trouble converting into json.  When we return
+			//sets of information, it's a good idea to create a custom container class to represent instances (or rows) of that information, and then return an array of those objects.  
+			//Keeps everything simple.
+
+			//WE ONLY SHARE ACCOUNTS WITH LOGGED IN USERS!
+			if (Session["id"] != null)
+			{
+				DataTable sqlDt = new DataTable("accounts");
+
+				string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+				string sqlSelect = "select id, userid, pass, firstname, lastname, email from accounts where active=1 order by lastname";
+
+				MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+				MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+				//gonna use this to fill a data table
+				MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+				//filling the data table
+				sqlDa.Fill(sqlDt);
+
+				//loop through each row in the dataset, creating instances
+				//of our container class Account.  Fill each account with
+				//data from the rows, then dump them in a list.
+				List<Account> accounts = new List<Account>();
+				for (int i = 0; i < sqlDt.Rows.Count; i++)
+				{
+					//only share user id and pass info with admins!
+					if (Convert.ToInt32(Session["admin"]) == 1)
+					{
+						accounts.Add(new Account
+						{
+							id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
+							userId = sqlDt.Rows[i]["userid"].ToString(),
+							password = sqlDt.Rows[i]["pass"].ToString(),
+							firstName = sqlDt.Rows[i]["firstname"].ToString(),
+							lastName = sqlDt.Rows[i]["lastname"].ToString(),
+							email = sqlDt.Rows[i]["email"].ToString()
+						});
+					}
+					else
+					{
+						accounts.Add(new Account
+						{
+							id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
+							firstName = sqlDt.Rows[i]["firstname"].ToString(),
+							lastName = sqlDt.Rows[i]["lastname"].ToString(),
+							email = sqlDt.Rows[i]["email"].ToString()
+						});
+					}
+				}
+				//convert the list of accounts to an array and return!
+				return accounts.ToArray();
+			}
+			else
+			{
+				//if they're not logged in, return an empty array
+				return new Account[0];
+			}
+		}
+
+	}
+}
